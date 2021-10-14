92506691d4ba3741fa4316f01d96d3f7addcb550
Anthony Snelling <anthony.snelling@live.com>
2021-10-14 19:02:39 -0400

    created hello world, added triangle

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -25,4 +25,25 @@ namespace HelloWorld
             }
         }
     }
+    public class Window : GameWindow
+        {
+            // A simple constructor to let us set properties like window size, title, FPS, etc. on the window.
+            public Window(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
+                : base(gameWindowSettings, nativeWindowSettings)
+            {
+            }
+    
+            // This function runs on every update frame.
+            protected override void OnUpdateFrame(FrameEventArgs e)
+            {
+                // Check if the Escape button is currently being pressed.
+                if (KeyboardState.IsKeyDown(Keys.Escape))
+                {
+                    // If it is, close the window.
+                    Close();
+                }
+    
+                base.OnUpdateFrame(e);
+            }
+        }
 }
