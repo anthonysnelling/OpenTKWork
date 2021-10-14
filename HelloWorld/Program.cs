53c4c92ab278cf1f9071cd76ab66974761cd2764
Anthony Snelling <anthony.snelling@live.com>
2021-10-14 18:16:45 -0400

    windows creation

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
 using OpenTK;
 using OpenTK.Graphics;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
 
 namespace HelloWorld
 {
-    class Program : GameWindow
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var nativeWindowSettings = new NativeWindowSettings()
+            {
+                Size = new Vector2i(1200, 720),
+                Title = "LearnOpenTk - Creating A Window",
+                Flags = ContextFlags.ForwardCompatible,
+            };
+
+            using (var window = new Window(GameWindowSettings.Default, nativeWindowSettings))
+            {
+                window.Run();
+            }
         }
     }
 }
