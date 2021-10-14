05ed3527b52bd2c96fdc6902ea6c16798044f6ae
Anthony Snelling <anthony.snelling@live.com>
2021-10-14 17:26:46 -0400

    initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Windowing.Desktop;
+
+namespace HelloWorld
+{
+    class Program : GameWindow
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
